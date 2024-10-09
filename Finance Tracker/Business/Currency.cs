2ae9d9bb38d1ae9c297c319e91d21b6c4e748fe3
usmaan786 <usmaanchohan78@gmail.com>
2024-10-09 02:24:11 +0100

    Add project files.

---
+++ Finance Tracker/Business/Currency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Finance_Tracker
+{
+    public class Currency
+    {
+        private static string apiKey = "0df2041ae1afede31d4a1e06";
+        private static string baseUrl = "https://v6.exchangerate-api.com/v6/";
+        public static async Task<decimal> ConvertCurrency(string initialCurrency,string targetCurrency, decimal amount)
+        {
+            string url = $"{baseUrl}{apiKey}/pair/{initialCurrency}/{targetCurrency}/{amount}";
+
+            var client = new RestClient();
+            var request = new RestRequest(url, Method.Get);
+            
+            var response = await client.ExecuteAsync(request);
+
+            if(response.IsSuccessful)
+            {
+                var jsonResponse = JObject.Parse(response.Content);
+                decimal conversionRate = jsonResponse["conversion_result"].Value<decimal>();
+                
+                return conversionRate;
+            }
+            else
+            {
+                throw new Exception("API Request Failed." + response.ErrorMessage);
+            }
+           
+        }
+    }
+}
---
+++ Finance Tracker/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance_Tracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> CurrencyList = new ObservableCollection<string>
+        {
+        "USD", "GBP", "EUR", "JPY", "AUD"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fromCurrenciesMenu.ItemsSource = CurrencyList;
+            toCurrenciesMenu.ItemsSource = CurrencyList;
+        }
+
+        private async void convertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string fromCurrency = fromCurrenciesMenu.SelectedItem as string;
+            string toCurrency = toCurrenciesMenu.SelectedItem as string;
+            
+            decimal amount = Decimal.Parse(fromCurrencyTxt.Text);
+
+            try
+            {
+                decimal convertedAmount = await Currency.ConvertCurrency(fromCurrency, toCurrency, amount);
+                toCurrencyTxt.Text = convertedAmount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
